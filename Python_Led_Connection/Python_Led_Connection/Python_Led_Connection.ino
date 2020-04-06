950e6a65ed9cd075837b04e3b74fc2e283835722
Ali Camdal <62968773+alicamdal@users.noreply.github.com>
2020-04-06 21:27:10 +0300

    Update Python_Led_Connection.ino

--- Python_Led_Connection/Python_Led_Connection/Python_Led_Connection.ino
+++ Python_Led_Connection/Python_Led_Connection/Python_Led_Connection.ino
@@ -1,11 +1,11 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial espSerial(3,2);
+SoftwareSerial espSerial(2,3);
 
 int led = 13;
 
-#define ssid "EmberSpirit"
-#define PASSWORD "19051905aA."
+#define ssid "YOUR_WiFi_SSID"
+#define PASSWORD "YOUR_WiFi_PASSWORD"
 
 String fmsg;
 String msg;
